0d1c8d162d6e98217224ce4d06e5a09f21086f37
김하린 <halin130@naver.com>
2022-07-12 17:30:16 +0900

    feat: add font, modify eslint rule

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,8 +1,26 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ['./src/**/*.{html,js}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          DEFAULT: '#f8d359' /* 메인바탕 */,
+          1: '#fffaeb' /* 메인 글씨 색 */,
+          2: '#fcf4d8;' /* 그릴 단어 생성 화면 글씨 색 */,
+          3: '#dbb750' /* 1인용 결과 페이지 버튼 색 */,
+          4: '#fff3cc' /* 메인 버튼 색 */,
+        },
+        gray: {
+          DEFAULT: '#ffffff' /* 기본 회색 */,
+          1: '#d9d9d9' /* 그래프 회색 부분 */,
+        },
+      },
+      fontFamily: {
+        poppins: ['Poppins', 'sans-serif'],
+        cookierun: ['COOKIERUN'],
+      },
+    },
   },
   plugins: [],
-}
+};
