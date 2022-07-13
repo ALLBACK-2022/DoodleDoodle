b8e1254aa0f6f0a573ab2a5aa53aae41458874ef
김하린 <halin130@naver.com>
2022-07-13 10:30:42 +0900

    Auto stash before rebase of "origin/feature/frontend-hdy-startpage"

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,8 @@
 import './App.css';
+import Random from './pages/Random';
 
 function App() {
-  return (
-    <div>
-      <nav>
-        <div className="logo">Tailwind CSS</div>
-        <ul>
-          <li>Install</li>
-          <li>Docs</li>
-        </ul>
-      </nav>
-      <div className="flex flex-col items-center mt-16">
-        <h1 className="text-4xl text-center">Tailwind CSS makes styling React components easier!</h1>
-        <button className="bg-black text-white p-2.5 w-fit mt-9 ">Get Started</button>
-      </div>
-    </div>
-  );
+  return <Random />;
 }
 
 export default App;
