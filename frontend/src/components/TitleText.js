9c4b12eb7225f214c2e0a379d92beb72ba156508
김하린 <halin130@naver.com>
2022-07-13 11:22:44 +0900

    feat: fix draft code with components

---
+++ frontend/src/components/TitleText.js
@@ -0,0 +1,12 @@
+function TitleText({ text }) {
+  return (
+    <h1
+      className="className = text-7xl text-primary-1 font-cookierun blur-none
+      drop-shadow-xl shadow-black"
+    >
+      {text}
+    </h1>
+  );
+}
+
+export default TitleText;
