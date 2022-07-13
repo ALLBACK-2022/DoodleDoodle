b510d98d0e220cd6933c088b21fc0b38b9d108aa
JeongHunHui <deca3248@gmail.com>
2022-07-14 02:41:44 +0900

    fix: fix GamePage.js and etc

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -11,32 +11,6 @@ root.render(
   </React.StrictMode>,
 );
 
-function Container() {
-  const [counter, setCounter] = React.useState(0);
-  function TestButtonClick() {
-    setCounter(current => current + 1);
-  }
-  return (
-    <div className="flex flex-col items-center">
-      <h1 className="text-4xl mt-16">Button Click Count: {counter}</h1>
-      <button className="bg-sky-600 hover:bg-sky-700 text-white p-2.5 w-fit mt-9" onMouseUp={TestButtonClick}>
-        Click This
-      </button>
-    </div>
-  );
-}
-
-function RenderScreen() {
-  root.render(
-    <React.StrictMode>
-      <App />
-      <Container />
-    </React.StrictMode>,
-  );
-}
-
-RenderScreen();
-
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
