99063e6cd4b62f40717a6600cb1151f63caa4ea1
김하린 <halin130@naver.com>
2022-07-13 22:39:50 +0900

    feat: fix Random.js

--- frontend/src/pages/Random.js
+++ frontend/src/pages/Random.js
@@ -3,8 +3,19 @@ import play from '../assets/icons/startImg.png';
 import ContentText from '../components/ContentText';
 import IconButton from '../components/IconButton';
 import TitleText from '../components/TitleText';
+import axios from 'axios';
+import useAsync from '../useAsync';
+
+async function getWord() {
+  const response = await axios.get('https://jsonplaceholder.typicode.com/users');
+  return response.data;
+}
 
 function Random() {
+  const [state, refetch] = useAsync(getWord, []);
+  const { loading, data: randword, error } = state;
+
+  if (error) return <div>에러가 발생했습니다</div>;
   return (
     <div className="m-0 w-screen h-screen bg-primary flex">
       <div className="m-auto flex flex-col items-center">
