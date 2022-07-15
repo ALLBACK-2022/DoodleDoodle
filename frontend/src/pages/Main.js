19d997937658fa2da9821001affb836ec827ffb5
Dayeon-Hong <dayon0325@gmail.com>
2022-07-15 11:00:55 +0900

    feat: add Main Page
    design 완료

--- frontend/src/pages/Main.js
+++ frontend/src/pages/Main.js
@@ -1,6 +1,7 @@
 import MainText from '../components/MainText';
 import AboutIcon from '../assets/icons/aboutIcon.png';
 import MainIcon from '../assets/icons/mainIcon.png';
+import NumIcon from '../components/NumIcon';
 
 /*
  *   '두들 두들!' 시작 페이지
@@ -10,17 +11,27 @@ function Main() {
   return (
     <div className="flex m-0 w-screen h-screen relative bg-primary">
       <div className="flex flex-wrap">
-        <img className="absolute left-12 top-12 w-14 h-14" src={AboutIcon} alt="" />
-        <img className=" absolute top-10 right-10  h-[35rem] " src={MainIcon} alt="" />
+        <button className="absolute left-12 top-12 w-14 h-14">
+          <img src={AboutIcon} alt="" />
+        </button>
+
+        <img className=" absolute mt-[13vh] right-10  h-[35rem]  " src={MainIcon} alt="" />
 
         <div className=" flex-col ml-[13vh] mt-[28vh] flex ">
           <MainText text=" 두들, 두들! " />
           <h2 className="className = mt-8 ml-52 text-5xl text-primary-1 font-cookierun doddleshadow textborder">
             Doodle, Doodle
           </h2>
-        </div>
 
-        <div className>start</div>
+          <div className="flex-row flex left-0 mt-[16vh] ml-[5.5vw]">
+            <div className="flex ">
+              <button className="text-6xl text-primary-1 font-cookierun startshadow textborder">start</button>
+              <div className="ml-20">
+                <NumIcon Num="2" />
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
