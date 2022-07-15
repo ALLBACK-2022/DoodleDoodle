39df7836671b34ea58df2871e7120b90766db52b
Dayeon-Hong <dayon0325@gmail.com>
2022-07-15 17:01:27 +0900

    feat: delete img

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,13 +2,14 @@ import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import Random from './pages/Random';
+import Main from './pages/Main';
 import ResultforOne from './pages/ResultforOne';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Random />} />
+        <Route path="/" element={<Main />} />
         <Route path="random" element={<Random />} />
         <Route path="resultone" element={<ResultforOne />} />
       </Routes>
