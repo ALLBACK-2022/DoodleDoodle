5bbf684e8fe1ecb5559dfd163a7b593e20f37453
ohksj77 <ohksj77@gmail.com>
2022-07-20 16:13:16 +0900

    chore: fix docker error

--- ai/server.py
+++ ai/server.py
@@ -18,8 +18,8 @@ with open("./ai-model/class_names.txt", "r") as ins:
     class_names.append(line.rstrip('\n'))
 
 # Load the model
-model = keras.models.load_model('./ai-model/doodleNet-model.h5')
-#model.summary()
+model = keras.models.load_model('./ai-model/keras.h5')
+# model.summary()
 
 @app.route("/AI", methods=['GET'])
 def index():
