20c28065e0d3a9500eaf1fe5abeda2fbb44d977d
YunHo Jung <a01049048063@gmail.com>
2022-07-20 15:07:06 +0900

    feat: add task entity

--- backend/models.py
+++ backend/models.py
@@ -86,8 +86,8 @@ class Draw(Base):
 
         
 
-class Word(Base):
-    __tablename__ = 'word'
+class Dictionary(Base):
+    __tablename__ = 'dictionary'
     __table_args__ = {'mysql_collate': 'utf8_general_ci'}
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(20))
@@ -132,6 +132,7 @@ class Result(Base):
     __tablename__ = 'result'
     __table_args__ = {'mysql_collate': 'utf8_general_ci'}
     id = db.Column(db.Integer, primary_key=True)
+    game_id = db.column(db.Integer)
     similarity = db.Column(db.Float)
     created_at = db.Column(db.DateTime)
     updated_at = db.Column(db.DateTime)
@@ -144,7 +145,19 @@ class Result(Base):
         self.created_at = datetime.datetime.now().replace(microsecond=0)
         self.updated_at = self.created_at
         
-        
+
+class Task(Base):
+    __tablename__ = 'task'
+    __table_args__ = {'mysql_collate': 'utf8_general_ci'}    
+    id = db.Column(db.Integer, primary_key=True)
+    status = db.Column(db.String(20))
+    created_at = db.Column(db.DateTime)
+    updated_at = db.Column(db.DateTime)
+
+    def __init__(self,status):
+        self.status = status
+        self.created_at = datetime.datetime.now().replace(microsecond=0)
+        self.updated_at = self.created_at
 
 
 Base.metadata.create_all(engine)
