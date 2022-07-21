540d9c2d3701b741b84f280fc9db6473103afa9f
Dayeon-Hong <dayon0325@gmail.com>
2022-07-21 13:16:10 +0900

    feat: fix game start button

--- frontend/src/components/RandomTitle.js
+++ frontend/src/components/RandomTitle.js
@@ -1,5 +1,5 @@
 function RandomTitle({ word }) {
   // eslint-disable-next-line react/no-unescaped-entities
-  return <h1 className="textshadow textborder text-8xl text-primary-1 font-cookierun">{`'${word}' 를 그려보세요!`}</h1>;
+  return <h1 className="textshadow textborder text-7xl text-primary-1 font-cookierun">{`'${word}' 를 그려보세요!`}</h1>;
 }
 export default RandomTitle;
