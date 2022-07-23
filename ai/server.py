eb7aa45ad92c88e7959bf1dc423c9ed16b4aa5f1
ohksj77 <ohksj77@gmail.com>
2022-07-23 19:09:25 +0900

    feat: add sharing function

---
+++ ai/server.py
@@ -0,0 +1,71 @@
+"""
+Flask Server with AI model
+"""
+from flask import Flask, request
+
+import os, keras, numpy as np, os
+
+from skimage.transform import resize
+import matplotlib.pyplot as plt
+from troposphere import Not
+
+from config import BUCKET_NAME
+from connection import s3_connection, s3_get_object
+
+app = Flask(__name__)
+
+# Read class names
+with open("ai-model/class_names.txt", "r") as ins:
+  class_names = []
+  for line in ins:
+    class_names.append(line.rstrip('\n'))
+
+# Load the model
+model = keras.models.load_model('./ai-model/keras.h5')
+# model.summary()
+
+@app.route("/AI", methods=['POST'])
+def index():
+    
+    if request.method =='POST':
+        value = request.get_json()
+        if not value:
+            return ('No parameter', 400)
+        #랜덤 단어와 이미지 url을 받아온다.
+        s3 = s3_connection()
+        if not os.path.exists('temp'):
+            os.mkdir('temp')
+        filepath = str(value['draw-id']) + '.png'
+        if s3_get_object(s3, BUCKET_NAME, 'drawimage/'+ filepath, 'temp/' + filepath):
+            ranword = request.args.get('ranword')
+            
+            img = plt.imread('temp/' + filepath)
+            img = img[:, :, 0]
+            img = resize(img, (28, 28))
+
+            for x in range(0, 28):
+                for y in range(0, 28):
+                    num = img[x][y] - 1
+                    if num < 0:
+                        num *= -1
+                    img[x][y] = num
+
+            pred = model.predict(np.expand_dims(img, axis=0))[0]
+            ind = (-pred).argsort()[:] # all sequence with similarity sorted by highest.  
+            otherResults={}
+            result={}
+            for x in range(0,len(ind)):
+                if(class_names[ind[x]]==ranword):
+                    result[ class_names[ind[x]] ] = str(round(pred[ind[x]]*100, 2))+'%'
+                if x<5:
+                    otherResults[ class_names[ind[x]] ] = str(round(pred[ind[x]]*100, 2)) + '%'
+
+            otherResults['result'] = result
+
+            os.remove('temp/' + filepath)
+            return (otherResults, 200)
+        return ("can't download file", 400)
+    return ("error", 400)
+
+if __name__ =='__main__':
+    app.run(port=5001, debug=True)
--- frontend/src/components/ShareResult.js
+++ frontend/src/components/ShareResult.js
@@ -1,67 +1,49 @@
-import React from 'react';
-import html2canvas from 'html2canvas';
-
-function ShareResult({ isforOne }) {
-  function download(dataurl, filename) {
-    const link = document.createElement('a');
-    link.href = dataurl;
-    link.download = filename;
-    link.click();
-  }
-
-  async function share() {
-    await html2canvas(document.getElementById('resultonepage')).then(async canvas => {
-      await canvas.toBlob(function (blob) {
-        download(URL.createObjectURL(blob), 'result.png');
-      }, 'image/png');
-    });
-  }
-
-  return (
-    <button
-      onClick={share}
-      className={`font-cookierun deskTop:text-[2vmin] mobile:text-[2vh]
-      deskTop:w-[10vw] deskTop:py-[1vh] deskTop:max-w-[15vh]
-      mobile:w-[11vh] mobile:max-w-[23vw] mobile:py-[1vh] rounded-full whitespace-nowrap
-      ${isforOne ? 'bg-primary-3 text-primary-1' : 'bg-black text-primary'}`}
-    >
-      결과저장
-    </button>
-  );
-}
-
-export default ShareResult;
-
-/*
 import React, {useRef} from 'react';
 import html2canvas from 'html2canvas';
 
 function ShareResult({isforOne}){
 
-    function download(dataurl, filename) {
-        const link = document.createElement("a");
-        link.href = dataurl;
-        link.download = filename;
-        link.click();
-    }
-
     async function share() {
+
+        const result = async (title, text, blob) => {
+
+            const data = {
+              files: [
+                new File([blob], 'result.png', {
+                  type: 'image/png',
+                }),
+              ],
+              title: title,
+              text: text,
+            };
+
+            try {
+              if (!navigator.canShare || !(navigator.canShare(data))) {
+                throw new Error("Can't share data.", data);
+              }
+              await navigator.share(data);
+            } catch (err) {
+              console.error(err.name, err.message);
+            }
+        };
 
         await html2canvas(document.getElementById("resultonepage")).then(async (canvas) => {
             await canvas.toBlob(function(blob) {
-                download(URL.createObjectURL(blob),"result.png")
+                result('reuslt', 'resultShare', blob)
             }, 'image/png');
         });
     }
 
     return (
             <button onClick={share}
-                className={`font-cookierun lg:text-2xl sm:text-lg px-[1.5rem] py-[0.3rem] rounded-full whitespace-nowrap
-                    ${isforOne ? 'bg-primary-3 text-primary-1' : 'bg-black text-primary'}`}
                 >
-                결과저장
+                자랑하기
             </button>
     );
 }
 
-export default ShareResult; */
+export default ShareResult;
+      className={`font-cookierun deskTop:text-[2vmin] mobile:text-[2vh]
+      deskTop:w-[10vw] deskTop:py-[1vh] deskTop:max-w-[15vh]
+      mobile:w-[11vh] mobile:max-w-[23vw] mobile:py-[1vh] rounded-full whitespace-nowrap
+      ${isforOne ? 'bg-primary-3 text-primary-1' : 'bg-black text-primary'}`}
