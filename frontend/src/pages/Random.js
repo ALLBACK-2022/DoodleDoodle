2c7d678e0ecbfcd672c1aa6bea586d7b62137afd
김하린 <halin130@naver.com>
2022-07-23 22:13:24 +0900

    feat: delete MediaQuery.js

--- frontend/src/pages/Random.js
+++ frontend/src/pages/Random.js
@@ -5,7 +5,6 @@ import ContentText from '../components/ContentText';
 import RandomTitle from '../components/RandomTitle';
 import RandomWordButton from '../components/RandomWordButton';
 import StartDrawButton from '../components/StartDrawButton';
-import { isPc, isMobile } from '../MediaQuery';
 
 /*
  *   '두들 두들!' 단어 선택 페이지
@@ -15,27 +14,22 @@ function Random() {
   const [word, setWord] = useState('');
 
   return (
-    <div>
-      {isPc && (
-        <div className="m-0 w-screen h-screen bg-primary flex">
-          <div className="m-auto flex flex-col items-center">
-            <div className="self-center">
-              <RandomTitle word={word} />
-            </div>
-            <div className="flex mt-80">
-              <div className="mr-64 flex flex-col items-center">
-                <ContentText text="다른 거 그리고 싶어요" />
-                <RandomWordButton image={recreate} setWord={setWord} />
-              </div>
-              <div className="ml-64 flex flex-col items-center">
-                <ContentText text="네!" />
-                <StartDrawButton image={play} word={word} />
-              </div>
-            </div>
+    <div className="m-0 w-screen h-screen bg-primary flex">
+      <div className="m-auto flex flex-col items-center">
+        <div className="self-center">
+          <RandomTitle word={word} />
+        </div>
+        <div className="flex mt-80">
+          <div className="mr-64 flex flex-col items-center">
+            <ContentText text="다른 거 그리고 싶어요" />
+            <RandomWordButton image={recreate} setWord={setWord} />
+          </div>
+          <div className="ml-64 flex flex-col items-center">
+            <ContentText text="네!" />
+            <StartDrawButton image={play} word={word} />
           </div>
         </div>
-      )}
-      {isMobile && <p>This is Mobile!</p>}
+      </div>
     </div>
   );
 }
