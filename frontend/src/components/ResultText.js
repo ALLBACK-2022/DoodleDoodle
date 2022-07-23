a71b2ccce29315e14c2f6eae5d2ebccdc478c8c1
ohksj77 <ohksj77@gmail.com>
2022-07-23 19:09:25 +0900

    feat: add sharing function

--- frontend/src/components/ResultText.js
+++ frontend/src/components/ResultText.js
@@ -1,4 +1,15 @@
-function ResultText({ text, textSize }) {
+function ResultText({ name, value, textSize }) {
+  let text = ''
+  if(value < 30){
+    text = `AI는 ${name}을 ${value}% 밖에 예측을 못했네요...`
+  }
+  else if(value < 60){
+    text = `AI는 ${name}을 ${value}% 정도로 예측했네요.`
+  }
+  else{
+    text = `AI는 ${name}을 ${value}% 나 예측했어요!`
+  }
+
   switch (textSize) {
     case 3:
       return <h1 className="lg:text-3xl sm:text-xl ont-blackf font-cookierun h-[12%] text-center mb-[3rem]">{text}</h1>;
--- frontend/src/pages/ResultforOne.js
+++ frontend/src/pages/ResultforOne.js
@@ -1,4 +1,4 @@
-import useState from 'react';
+import {useState} from 'react'
 import ResultText from '../components/ResultText';
 import gameBGImg from '../assets/icons/gameBGImg.png';
 import sketchbook from '../assets/icons/sketchbook.png';
@@ -6,18 +6,18 @@ import ResultPieChart from '../components/ResultPieChart';
 import PieChartLabel from '../components/PieChartLabel';
 import ResultButtons from '../components/ResultButtons';
 import ResultSimilarity from '../components/ResultSimilarity';
-import ResultImage from '../components/ResultImage';
+import ResultImage from '../components/ResultImage'
 
 function ResultforOne() {
-  const [chartData, setChartData] = useState([{ name: '', value: 0.0 }]);
-  const [randomWordData, setRandomWordData] = useState({ name: '', value: 0.0 });
-  const [imageUrl, setImageUrl] = useState([{ url: '' }]);
+  const [chart, setChart] = useState([{ name: '', value: 0.0 },])
+  const [randomWordData, setRandomWordData] = useState({ name: '', value: 0.0 })
+  const [imageUrl, setImageUrl] = useState([{url: ''},])
 
   // 여기서 POST API 불러오기(각 데이터 넣어주기)
-  ResultSimilarity(setChartData, setRandomWordData, setImageUrl);
-
+  ResultSimilarity(setChart, setRandomWordData, setImageUrl)
+  
   return (
-    <div className="flex relative w-full h-full bg-primary">
+    <div id="resultonepage" className="flex relative w-full h-full bg-primary">
       <img id="gameBGImg" src={gameBGImg} className="px-[2vw] py-[2vh] w-screen h-screen" alt="" />
 
       <div className="flex absolute px-[8vw] py-[8vh] w-screen h-screen flex-col">
@@ -25,29 +25,29 @@ function ResultforOne() {
         <div className="flex flex-row ml-[4vw] mt-[6vh] items-stretch">
           <div className="w-[45%] sm:h-[30%] md:h-[60%] lg:h-[100%]">
             <div className="relative inline-flex flex-col w-[50%] h-[60%]">
-              <ResultPieChart chartData={chartData[0].value} />
+              <ResultPieChart chartData={chart[0].value} />
               <ResultImage imageUrl={imageUrl[0]} />
-              <PieChartLabel text={chartData[0].name} />
+              <PieChartLabel text={chart[0].name} />
             </div>
             <div className="inline-flex flex-col w-[25%] h-[32%] -translate-y-[80%] translate-x-[20%]">
-              <ResultPieChart chartData={chartData[1].value} />
+              <ResultPieChart chartData={chart[1].value} />
               <ResultImage imageUrl={imageUrl[1]} />
-              <PieChartLabel text={chartData[2].name} />
+              <PieChartLabel text={chart[1].name} />
             </div>
             <div className="inline-flex flex-col w-[25%] h-[32%] -translate-x-[100%] translate-y-[30%]">
-              <ResultPieChart chartData={chartData[2].value} />
+              <ResultPieChart chartData={chart[2].value} />
               <ResultImage imageUrl={imageUrl[2]} />
-              <PieChartLabel text={chartData[3].name} />
+              <PieChartLabel text={chart[2].name} />
             </div>
             <div className="inline-flex flex-col w-[25%] h-[32%] translate-y-[15%]">
-              <ResultPieChart chartData={chartData[3].value} />
+              <ResultPieChart chartData={chart[3].value} />
               <ResultImage imageUrl={imageUrl[3]} />
-              <PieChartLabel text={chartData[4].name} />
+              <PieChartLabel text={chart[3].name} />
             </div>
             <div className="inline-flex flex-col w-[25%] h-[32%] translate-x-[20%] translate-y-[15%]">
-              <ResultPieChart chartData={chartData[4].value} />
-              <ResultImage imageUrl={imageUrl[5]} />
-              <PieChartLabel text="지우개" />
+              <ResultPieChart chartData={chart[4].value} />
+              <ResultImage imageUrl={imageUrl[4]} />
+              <PieChartLabel text={chart[4].name} />
             </div>
           </div>
           <div className="w-[45%] ml-[3vw] flex flex-col justify-items-centers">
@@ -60,10 +60,7 @@ function ResultforOne() {
               />
             </div>
 
-            <ResultText
-              text={`AI는 ${randomWordData.name}를 ${randomWordData.value}%밖에 예측을 못했네요..`}
-              textSize={3}
-            />
+            <ResultText name={randomWordData.name} value={randomWordData.value} textSize={3} />
             <div>
               <ResultButtons isforOne />
             </div>
