746649f0e48e115051f1776f5423dbe856576d24
ohksj77 <ohksj77@gmail.com>
2022-07-24 13:02:21 +0900

    fix: change sharing to download img and add dockerfile for celery

--- frontend/src/components/ShareResult.js
+++ frontend/src/components/ShareResult.js
@@ -37,33 +37,40 @@ import html2canvas from 'html2canvas';
 
 function ShareResult({isforOne}){
 
+    function download(dataurl, filename) {
+        const link = document.createElement("a");
+        link.href = dataurl;
+        link.download = filename;
+        link.click();
+    }
+
     async function share() {
 
-        const result = async (title, text, blob) => {
+        // const result = async (title, text, blob) => {
 
-            const data = {
-              files: [
-                new File([blob], 'result.png', {
-                  type: 'image/png',
-                }),
-              ],
-              title: title,
-              text: text,
-            };
+        //     const data = {
+        //       files: [
+        //         new File([blob], 'result.png', {
+        //           type: 'image/png',
+        //         }),
+        //       ],
+        //       title: title,
+        //       text: text,
+        //     };
 
-            try {
-              if (!navigator.canShare || !(navigator.canShare(data))) {
-                throw new Error("Can't share data.", data);
-              }
-              await navigator.share(data);
-            } catch (err) {
-              console.error(err.name, err.message);
-            }
-        };
+        //     try {
+        //       if (!navigator.canShare || !(navigator.canShare(data))) {
+        //         throw new Error("Can't share data.", data);
+        //       }
+        //       await navigator.share(data);
+        //     } catch (err) {
+        //       console.error(err.name, err.message);
+        //     }
+        // };
 
         await html2canvas(document.getElementById("resultonepage")).then(async (canvas) => {
             await canvas.toBlob(function(blob) {
-                result('reuslt', 'resultShare', blob)
+                download(URL.createObjectURL(blob),"result.png")
             }, 'image/png');
         });
     }
@@ -73,7 +80,7 @@ function ShareResult({isforOne}){
                 className={`font-cookierun lg:text-2xl sm:text-lg px-[1.5rem] py-[0.3rem] rounded-full whitespace-nowrap
                     ${isforOne ? 'bg-primary-3 text-primary-1' : 'bg-black text-primary'}`}
                 >
-                자랑하기
+                결과저장
             </button>
     );
 }
