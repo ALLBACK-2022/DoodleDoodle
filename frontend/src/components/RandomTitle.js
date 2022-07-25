170d332acd2ec40cc494250deae3115535e35548
김하린 <halin130@naver.com>
2022-07-25 13:40:14 +0900

    feat: add doodleicons, mobile random page

--- frontend/src/components/RandomTitle.js
+++ frontend/src/components/RandomTitle.js
@@ -5,6 +5,7 @@ function RandomTitle({ word, isMobile }) {
       <h1
         className="randomshadow textborder text-[3.125rem] text-primary-1 
 <<<<<<< develop
+<<<<<<< develop
   font-cookierun_m whitespace-pre text-center leading-relaxed"
       >{`'${word}' 를
 그려보세요!`}</h1>
@@ -12,6 +13,11 @@ function RandomTitle({ word, isMobile }) {
   font-cookierun_m whitespace-normal text-center leading-relaxed"
       >{`'${word}' 를 그려보세요!`}</h1>
 >>>>>>> feat: start mobile randompage
+=======
+  font-cookierun_m whitespace-pre text-center leading-relaxed"
+      >{`'${word}' 를
+그려보세요!`}</h1>
+>>>>>>> feat: add doodleicons, mobile random page
     );
   }
   return (
