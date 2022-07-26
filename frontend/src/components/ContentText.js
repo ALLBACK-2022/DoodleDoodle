36c24b219147d00439c482c0fcc1e2c3c768b868
김하린 <halin130@naver.com>
2022-07-26 15:26:46 +0900

    feat: add mobile multiresult component

--- frontend/src/components/ContentText.js
+++ frontend/src/components/ContentText.js
@@ -1,5 +1,5 @@
 function ContentText({ text }) {
-  return <div className="mb-6 text-5xl font-black text-primary-1 font-cookierun">{text}</div>;
+  return <div className="mb-6 text-5xl font-black text-primary-1 font-cookierun text-center">{text}</div>;
 }
 
 export default ContentText;
