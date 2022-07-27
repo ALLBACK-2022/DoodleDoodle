fc8c275c6e102150b6088c4528086ed3eb2d020e
Dayeon-Hong <dayon0325@gmail.com>
2022-07-27 11:47:48 +0900

    feat: resultmany page
    페이지 구성 -> 미완( 스케치북 6개일떄 화면만 어느정도 구현했고, 스케치북수가 줄었을떄 스케치북 크기를 늘리지 못함, 플레이어 수와의 연동도 X

---
+++ frontend/src/components/ResultRank.js
@@ -0,0 +1,26 @@
+// import React, { useState } from 'react';
+import sketchbook from '../assets/icons/sketchbook.png';
+// import example from '../assets/icons/plus.png';
+
+function ResultRank() {
+  // 여기서 GET API 불러오기(각 데이터 넣어주기)
+  //   const chartData = [{percentage, rank, doodle, player }]
+
+  return (
+    <div className="flex ">
+      <div className="flex flex-col m-[2rem]">
+        <div className="absolute flex flex-row space-x-10">
+          <div className="font-cookierun text-5xl text-black ml-3 mr-[4rem] "> 1st </div>
+          <div className="font-cookierun text-3xl text-black "> player2 </div>
+          <div className="font-cookierun text-3xl text-white "> 90% </div>
+        </div>
+        <div>
+          <img id="sketchbook" src={sketchbook} className=" mt-5  h-[18rem] relative " alt="" />
+          {/* <img id="example-pic" src={example} className="  absolute w-[10]  " alt="" /> */}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default ResultRank;
