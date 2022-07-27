4c787b85a134b0accc3e75a82fbd8d3b595849b0
Dayeon-Hong <dayon0325@gmail.com>
2022-07-27 13:35:07 +0900

    feat:fix resultmany page

--- frontend/src/components/ResultRank.js
+++ frontend/src/components/ResultRank.js
@@ -9,12 +9,12 @@ function ResultRank() {
     <div className="flex ">
       <div className="flex flex-col m-[2rem]">
         <div className="absolute flex flex-row space-x-10">
-          <div className="font-cookierun text-5xl text-black ml-3 mr-[4rem] "> 1st </div>
-          <div className="font-cookierun text-3xl text-black "> player2 </div>
-          <div className="font-cookierun text-3xl text-white "> 90% </div>
+          <div className="font-cookierun text-4xl text-black ml-3 mr-[4rem] "> 1st </div>
+          <div className="font-cookierun text-2xl text-black "> player2 </div>
+          <div className="font-cookierun text-2xl text-white "> 90% </div>
         </div>
         <div>
-          <img id="sketchbook" src={sketchbook} className=" mt-5  h-[18rem] relative " alt="" />
+          <img id="sketchbook" src={sketchbook} className=" mt-5  h-[16rem] relative " alt="" />
         </div>
       </div>
     </div>
