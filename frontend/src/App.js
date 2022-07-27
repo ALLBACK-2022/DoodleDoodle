cc93d5d49efaedfed46fa044a8cd1f097f025a11
김하린 <halin130@naver.com>
2022-07-27 14:50:49 +0900

    feat: add information page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,12 +7,14 @@ import GamePage from './pages/GamePage';
 import ResultforOne from './pages/ResultforOne';
 // import Temp from './pages/Temp'; // AI API연결 없이 1인용 결과페이지 테스트할때 사용
 import ResultMany from './pages/ResultMany';
+import About from './pages/About';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Main />} />
+        <Route path="/about" element={<About />} />
         <Route path="random" element={<Random />} />
         <Route path="gamepage" element={<GamePage />} />
         <Route path="resultone" element={<ResultforOne />} />
