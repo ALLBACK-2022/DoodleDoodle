8d204c58306bd33263ded1819fa5223c6c0fca4e
김하린 <halin130@naver.com>
2022-07-27 14:50:49 +0900

    feat: add information page

---
+++ frontend/src/components/Profile.js
@@ -0,0 +1,19 @@
+import profile from '../assets/icons/user.png';
+
+function Profile({ name, url }) {
+  return (
+    <div className="flex flex h-[100%] w-1/3 items-center justify-center">
+      <a href={url} target="_blank" rel="noreferrer">
+        <img className="deskTop:w-[4rem] deskTop:h-[4rem] mobile:w-[2.5rem] mobile:h-[2.5rem]" src={profile} alt="" />
+      </a>
+      <div
+        className="font-cookierun_s ml-[0.5rem] w-[3rem] h-[3rem] text-center 
+      deskTop:text-lg mobile:text-sm"
+      >
+        {name}
+      </div>
+    </div>
+  );
+}
+
+export default Profile;
