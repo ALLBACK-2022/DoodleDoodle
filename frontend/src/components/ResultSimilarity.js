cfd27cd03c8f18b57430e137d5df4e2dffa8c43f
ohksj77 <ohksj77@gmail.com>
2022-07-27 16:31:15 +0900

    feat: create database automatically

--- frontend/src/components/ResultSimilarity.js
+++ frontend/src/components/ResultSimilarity.js
@@ -1,6 +1,6 @@
 import { useLocation } from 'react-router';
 
-const baseURL = 'http://localhost:5000/api/draws/results';
+const baseURL = 'http://localhost:5000/api/v1/draws/results';
 function ResultSimilarity({setChartData, setRandomWordData, setImageUrl}){
     const location = useLocation();
     const array = []
