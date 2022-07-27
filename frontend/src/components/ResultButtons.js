e41acf4a28e18578ac5cb9c0993fca6cdecebd29
김하린 <halin130@naver.com>
2022-07-28 06:47:12 +0900

    feat: result for multi UI complete

--- frontend/src/components/ResultButtons.js
+++ frontend/src/components/ResultButtons.js
@@ -3,11 +3,23 @@ import ShareResult from './ShareResult';
 
 function ResultButtons({ isforOne }) {
   return (
+<<<<<<< develop
     <div className="flex flex-row justify-center">
       <ShareResult isforOne={isforOne}/>
+=======
+    <div className="inline-flex flex-row justify-center">
+      <button
+        className={`font-cookierun deskTop:text-2xl mobile:text-lg px-[1.5rem] 
+        py-[0.3rem] rounded-full whitespace-nowrap
+      ${isforOne ? 'bg-primary-3 text-primary-1' : 'bg-black text-primary'}`}
+      >
+        자랑하기
+      </button>
+>>>>>>> feat: result for multi UI complete
       <Link to="/" className="mx-[5%]">
         <button
-          className={`font-cookierun lg:text-2xl sm:text-lg px-[1.5rem] py-[0.3rem] rounded-full whitespace-nowrap
+          className={`font-cookierun deskTop:text-2xl mobile:text-lg px-[1.5rem] 
+          py-[0.3rem] rounded-full whitespace-nowrap
       ${isforOne ? 'bg-primary-3 text-primary-1' : 'bg-black text-primary'}`}
         >
           다시하기
@@ -15,7 +27,8 @@ function ResultButtons({ isforOne }) {
       </Link>
       <Link to="/">
         <button
-          className={`font-cookierun lg:text-2xl sm:text-lg px-[1.5rem] py-[0.3rem] rounded-full whitespace-nowrap
+          className={`font-cookierun deskTop:text-2xl mobile:text-lg px-[1.5rem] 
+          py-[0.3rem] rounded-full whitespace-nowrap
       ${isforOne ? 'bg-primary-3 text-primary-1' : 'bg-black text-primary'}`}
         >
           홈으로
