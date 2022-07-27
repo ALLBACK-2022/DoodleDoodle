1ebc3b00ce8620b13cb9133a6f6ba0cc261413fc
Dayeon-Hong <dayon0325@gmail.com>
2022-07-27 13:35:07 +0900

    feat:fix resultmany page

--- frontend/src/pages/ResultMany.js
+++ frontend/src/pages/ResultMany.js
@@ -1,4 +1,4 @@
-import ResultText from '../components/ResultText';
+// import ResultText from '../components/ResultText';
 
 import BGyelllowImg from '../assets/icons/BGyellowImg.png';
 import ResultButtons from '../components/ResultButtons';
@@ -7,11 +7,17 @@ import ResultRank from '../components/ResultRank';
 function ResultMany() {
   return (
     <div className="flex relative w-full h-full bg-primary-1">
-      <img id="gameBGImg" src={BGyelllowImg} className="px-[2vw] py-[2vh] w-screen h-screen" alt="" />
+      <img id="BGyellowImg" src={BGyelllowImg} className="absolute px-[2vw] py-[2vh] w-screen h-screen" alt="" />
 
-      <div className="flex absolute px-[8vw] py-[12vh] w-screen h-screen flex-col">
-        <ResultText text="누가 더 똑같이 그렸을까요?" />
-        <div className="flex mt-10 mx-1 mb-30 ">
+      <div className="flex absolute px-[8vw] py-[8vh] w-screen h-screen flex-col">
+        <h1
+          className="ml-[1%] text-black font-cookierun text-left
+        deskTop:text-[3.5vw] mobile:text-[6vw]"
+        >
+          AI는 무엇으로 예측했을까요?
+        </h1>
+
+        <div className="flex mt-3 mx-1 mb-30 ">
           <div className=" flex flex-wrap mx-10">
             {/* 구현하지 못한 부분 : 스케치북 개수 = player수  */}
             <ResultRank />
@@ -21,7 +27,7 @@ function ResultMany() {
           </div>
         </div>
 
-        <div className="flex flex-row mt-[3vh] mr-[10rem] mb-[1vh] items-stretch">
+        <div className="flex flex-row mt-[3vh] mr-[10rem] mb-[10vh] items-stretch">
           {/* 구현 하지 못한 부분: 버튼 오른쪽 아래 위치로 이동 */}
           <div>
             <ResultButtons isforOne2 />
