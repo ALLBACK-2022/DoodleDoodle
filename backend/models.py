952f5e318c1a4c9c83659649203c808ce5e99918
김하린 <halin130@naver.com>
2022-07-28 13:47:32 +0900

    feat: fix models.py

--- backend/models.py
+++ backend/models.py
@@ -1,5 +1,7 @@
 from flask_sqlalchemy import SQLAlchemy
-import datetime, os, pymysql
+import datetime
+import os
+import pymysql
 from flask import Flask
 from dotenv import load_dotenv
 from sqlalchemy import create_engine
@@ -9,14 +11,17 @@ from dotenv import load_dotenv
 app = Flask(__name__)
 load_dotenv()
 db = SQLAlchemy()
-MYSQL_ROOT_PASSWORD=os.environ.get("MYSQL_ROOT_PASSWORD")
-MYSQL_HOST=os.environ.get("MYSQL_HOST")
-sqlurl = 'mysql+pymysql://root:' + MYSQL_ROOT_PASSWORD + '@' + MYSQL_HOST + ':3306/DoodleDoodle'
+MYSQL_ROOT_PASSWORD = os.environ.get("MYSQL_ROOT_PASSWORD")
+MYSQL_HOST = os.environ.get("MYSQL_HOST")
+sqlurl = 'mysql+pymysql://root:' + MYSQL_ROOT_PASSWORD + \
+    '@' + MYSQL_HOST + ':3306/DoodleDoodle'
 Base = declarative_base()
 engine = create_engine(sqlurl)
 app.config['SQLALCHEMY_DATABASE_URI'] = sqlurl
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db.init_app(app)
+
+
 class Game(Base):
     __tablename__ = 'game'
     __table_args__ = {'mysql_collate': 'utf8_general_ci'}
@@ -26,18 +31,17 @@ class Game(Base):
     created_at = db.Column(db.DateTime)
     updated_at = db.Column(db.DateTime)
     draw = db.relationship('Draw', backref='game')
-    
+
     def __init__(self, random_word, player_num):
         self.random_word = random_word
         self.player_num = player_num
         self.created_at = datetime.datetime.now().replace(microsecond=0)
         self.updated_at = self.created_at
-        
-    
+
     def serialize(self):
         return {
-        "id" : self.id
-    }
+            "id": self.id
+        }
 
 
 class Draw(Base):
@@ -49,16 +53,15 @@ class Draw(Base):
     created_at = db.Column(db.DateTime)
     updated_at = db.Column(db.DateTime)
     game_id = db.Column(db.Integer, db.ForeignKey(Game.id))
-    
+    result = db.relationship('Result', backref='draw')
 
-
-    def __init__(self, draw_no, doodle,game_id):
+    def __init__(self, draw_no, doodle, game_id):
         self.draw_no = draw_no
         self.doodle = doodle
         self.game_id = game_id
         self.created_at = datetime.datetime.now().replace(microsecond=0)
         self.updated_at = self.created_at
-            
+
 
 class Dictionary(Base):
     __tablename__ = 'dictionary'
@@ -68,21 +71,20 @@ class Dictionary(Base):
     eng_name = db.Column(db.String(50))
     img_url = db.Column(db.Text)
     result = db.relationship('Result', backref='dictionary')
-    
+
     def __init__(self, name, eng_name, img_url):
         self.name = name
         self.eng_name = eng_name
         self.img_url = img_url
         self.created_at = datetime.datetime.now().replace(microsecond=0)
         self.updated_at = self.created_at
-        
- 
+
     def serialize(self):
         return {
-            "id" : self.id,
-            "name" : self.name,
-            "eng_name" : self.eng_name,
-            "img_url" : self.img_url
+            "id": self.id,
+            "name": self.name,
+            "eng_name": self.eng_name,
+            "img_url": self.img_url
         }
 
 
@@ -97,32 +99,27 @@ class Result(Base):
     dictionary_id = db.Column(db.Integer, db.ForeignKey(Dictionary.id))
     game_id = db.Column(db.Integer, db.ForeignKey(Game.id))
 
-     
     def __init__(self, similarity):
         self.similarity = similarity
         self.created_at = datetime.datetime.now().replace(microsecond=0)
         self.updated_at = self.created_at
 
 
-
 class Task(Base):
     __tablename__ = 'task'
-    __table_args__ = {'mysql_collate': 'utf8_general_ci'}    
+    __table_args__ = {'mysql_collate': 'utf8_general_ci'}
     id = db.Column(db.Integer, primary_key=True)
     status = db.Column(db.String(20))
     created_at = db.Column(db.DateTime)
     updated_at = db.Column(db.DateTime)
 
-
-    def __init__(self,status):
+    def __init__(self, status):
         self.status = status
         self.created_at = datetime.datetime.now().replace(microsecond=0)
         self.updated_at = self.created_at
 
-
     def set_updated_at(self):
         self.updated_at = datetime.datetime.now().replace(microsecond=0)
-
 
 
 Base.metadata.create_all(engine)
