b119cde2dc90285646bc4c7c48f4e30e0461d972
Dayeon-Hong <dayon0325@gmail.com>
2022-07-30 00:47:37 +0900

    feat: update animation

--- frontend/src/components/DoodleBelt.js
+++ frontend/src/components/DoodleBelt.js
@@ -1,33 +1,35 @@
-import doodle1 from '../assets/icons/mobiledoodle_1.png';
-import doodle6 from '../assets/icons/mobiledoodle_6.png';
-import doodle7 from '../assets/icons/mobiledoodle_7.png';
-import doodle8 from '../assets/icons/mobiledoodle_8.png';
-import doodle9 from '../assets/icons/mobiledoodle_9.png';
-import doodle10 from '../assets/icons/mobiledoodle_10.png';
-import doodle11 from '../assets/icons/mobiledoodle_11.png';
-import doodle12 from '../assets/icons/mobiledoodle_12.png';
+import doodle1 from '../assets/icons/mobiledoodle_13.png';
+import doodle2 from '../assets/icons/mobiledoodle_6.png';
+import doodle3 from '../assets/icons/mobiledoodle_7.png';
+import doodle4 from '../assets/icons/mobiledoodle_8.png';
+import doodle5 from '../assets/icons/mobiledoodle_9.png';
+import doodle6 from '../assets/icons/mobiledoodle_10.png';
+import doodle7 from '../assets/icons/mobiledoodle_11.png';
+import doodle8 from '../assets/icons/mobiledoodle_12.png';
+import doodle9 from '../assets/icons/mobiledoodle_14.png';
 
 function Doodle({ doodle }) {
-  return <img src={doodle.src} className="w-[12vw]" alt="" />;
+  return <img src={doodle.src} className="w-[13vw]" alt="" />;
 }
 
 function DoodleBelt() {
   const doodles = [
     { id: 1, src: doodle1 },
-    { id: 2, src: doodle6 },
-    { id: 3, src: doodle7 },
-    { id: 4, src: doodle8 },
-    { id: 5, src: doodle9 },
-    { id: 6, src: doodle10 },
-    { id: 7, src: doodle11 },
-    { id: 8, src: doodle12 },
+    { id: 2, src: doodle2 },
+    { id: 3, src: doodle3 },
+    { id: 4, src: doodle4 },
+    { id: 5, src: doodle5 },
+    { id: 6, src: doodle6 },
+    { id: 7, src: doodle7 },
+    { id: 8, src: doodle8 },
+    { id: 9, src: doodle9 },
   ];
 
   return (
     <div className="relative overflow-hidden">
       <div
         className="flex flex-row space-x-[10vw] w-[100%]
-          h-100% m-[3rem] animate-slider items-center"
+       h-100 m-[3rem] animate-slider items-center"
       >
         {doodles.map(doodle => (
           <Doodle doodle={doodle} key={doodle.id} />
@@ -39,80 +41,72 @@ function DoodleBelt() {
     </div>
   );
 
-  // 애니메이션 미적용 코드(prop으로 isTop추가)
-  /*
-=======
-=======
->>>>>>> backend-khl
-  return <img src={doodle.src} alt="" />;
-}
+  // const doodlesTop = [
+  //   { id: 1, src: doodle1 },
+  //   { id: 2, src: doodle2 },
+  //   { id: 3, src: doodle3 },
+  //   { id: 4, src: doodle4 },
+  //   { id: 5, src: doodle5 },
+  //   { id: 6, src: doodle6 },
+  //   { id: 7, src: doodle7 },
+  //   { id: 8, src: doodle8 },
+  //   { id: 9, src: doodle9 },
+  // ];
 
-function DoodleBelt({ isTop }) {
-<<<<<<< HEAD
->>>>>>> feat: add doodleicons, mobile random page
-=======
->>>>>>> backend-khl
-  const doodlesTop = [
-    {
-      id: 1,
-      src: doodle1,
-    },
-    { id: 2, src: doodle2 },
-    { id: 3, src: doodle3 },
-    { id: 4, src: doodle4 },
-  ];
-<<<<<<< HEAD
-<<<<<<< develop
+  // const doodlesBottom = [
+  //   { id: 1, src: doodle9 },
+  //   { id: 2, src: doodle1 },
+  //   { id: 3, src: doodle2 },
+  //   { id: 4, src: doodle3 },
+  //   { id: 5, src: doodle4 },
+  //   { id: 6, src: doodle5 },
+  //   { id: 7, src: doodle6 },
+  //   { id: 8, src: doodle7 },
+  //   { id: 9, src: doodle8 },
+  // ];
 
-=======
->>>>>>> feat: add doodleicons, mobile random page
-=======
->>>>>>> backend-khl
-  const doodlesBottom = [
-    {
-      id: 1,
-      src: doodle5,
-    },
-    { id: 2, src: doodle6 },
-    { id: 3, src: doodle7 },
-    { id: 4, src: doodle8 },
-  ];
+  // if (isMainPage) {
+  //   return (
+  //     <div className="relative overflow-hidden">
+  //       <div
+  //         className="flex flex-row space-x-[10vw] w-[100%]
+  //       my-[3vh] items-center animate-slider"
+  //       >
+  //         {doodles.map(doodle => (
+  //           <Doodle doodle={doodle} key={doodle.id} />
+  //         ))}
+  //         {doodles.map(doodle => (
+  //           <Doodle doodle={doodle} key={doodle.id} />
+  //         ))}
+  //       </div>
+  //     </div>
+  //   );
+  // }
 
-<<<<<<< HEAD
-<<<<<<< develop
-  if(isTop){
-    return (
-    <div className="flex flex-row h-100% place-content-around mt-[3rem]">
-      {doodlesTop.map(doodle => (
-        <Doodle doodle={doodle} key={doodle.id} />
-      ))}
-    </div>
-=======
-=======
->>>>>>> backend-khl
-  if (isTop) {
-    return (
-      <div className="flex flex-row h-100% place-content-around mt-[3rem]">
-        {doodlesTop.map(doodle => (
-          <Doodle doodle={doodle} key={doodle.id} />
-        ))}
-      </div>
-<<<<<<< HEAD
->>>>>>> feat: add doodleicons, mobile random page
-=======
->>>>>>> backend-khl
-    );
-  }
-  return (
-    <div className="flex flex-row h-100% place-content-around mb-[3rem]">
-      {doodlesBottom.map(doodle => (
-        <Doodle doodle={doodle} key={doodle.id} />
-      ))}
-    </div>
-  );
-<<<<<<< HEAD
-<<<<<<< develop
-  */
+  // if (isTop) {
+  //   return (
+  //     <div className="relative overflow-hidden">
+  //       <div
+  //         className="flex flex-row space-x-[10vw] w-[100%]
+  //         h-100% m-[3rem] animate-slider items-center"
+  //       >
+  //         {doodlesTop.map(doodle => (
+  //           <Doodle doodle={doodle} key={doodle.id} />
+  //         ))}
+  //         {/* {doodles.map(doodle => (
+  //         <Doodle doodle={doodle} key={doodle.id} />
+  //       ))} */}
+  //       </div>
+  //     </div>
+  //   );
+  // }
+  // return (
+  //   <div className="flex flex-row space-x-[10vw] w-[100%] mb-[3vh] items-center animate-slider">
+  //     {doodlesBottom.map(doodle => (
+  //       <Doodle doole={doodle} key={doodle.id} />
+  //     ))}
+  //   </div>
+  // );
 }
 
 export default DoodleBelt;
