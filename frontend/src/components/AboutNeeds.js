0659825b0eedcf5d1a7a623c4f72692fcb80c6e8
Dayeon-Hong <dayon0325@gmail.com>
2022-07-29 20:27:01 +0900

    feat: update aboutpage #2

--- frontend/src/components/AboutNeeds.js
+++ frontend/src/components/AboutNeeds.js
@@ -1,7 +1,7 @@
 function AboutNeeds({ text }) {
   return (
     <div
-      className="font-cookierun_s rounded-3xl border-4 border-dashed border-primary whitespace-pre-line
+      className="font-cookierun_s rounded-3xl border-4 border-dashed border-primary whitespace-pre-wrap
           h-[100%] mobile:w-[10.5rem] deskTop:w-[16rem] flex text-center  justify-content-center 
           items-center mobile:text-xl deskTop:text-xl"
     >
