228db8535c12015c77f2891fad645bebdf441ff0
Dayeon-Hong <dayon0325@gmail.com>
2022-07-30 21:58:28 +0900

    feat: change mobile button
    공유하기 제거, 뒤로가기 홈으로 변경

--- frontend/src/components/MobileBottomBtn.js
+++ frontend/src/components/MobileBottomBtn.js
@@ -1,11 +1,16 @@
 import { useNavigate } from 'react-router';
-import home from '../assets/icons/home2.png';
-import share from '../assets/icons/share2.png';
-import back from '../assets/icons/back.png';
-import changeImg from '../assets/icons/changeImg.png';
+import { Link } from 'react-router-dom';
+
+import home from '../assets/icons/mobile-home.png';
+import back from '../assets/icons/mobile-back.png';
+import restart from '../assets/icons/mobile-again.png';
+
+// 다시하기, 홈으로 수정 필요
 
 function MobileBottomBtn({ goback, playerNumber, gameId }) {
   const navigate = useNavigate();
+
+  // 다시하기
   function onClick() {
     navigate('../random', {
       replace: true,
@@ -17,48 +22,34 @@ function MobileBottomBtn({ goback, playerNumber, gameId }) {
     return (
       <div className="flex w-[80%] items-center self-center place-content-center place-content-between">
         <div className="flex flex-col items-center  space-y-[0.3rem]">
-          <button className="h-[2.4rem] w-[2.4rem]">
-            <img className="h-[2.4rem] w-[2.4rem]" src={share} alt="" />
+          <button className="h-[3rem] w-[3rem] ml-[17rem]">
+            <img className="h-[3rem] w-[3rem]" src={back} alt="" /> {/* 뒤로가기 */}
           </button>
-
-          <div className="font-cookierun_m text-[1rem]">저장하기</div>
         </div>
-        <div className="flex flex-col items-center  space-y-[0.3rem]">
-          <button className="h-[2.4rem] w-[2.4rem]">
-            <img className="h-[2rem] w-[2rem]" src={back} alt="" />
-          </button>
-          <div className="font-cookierun_m text-[1rem]">뒤로가기</div>
-        </div>
-        <div className="flex flex-col items-center  space-y-[0.3rem]">
-          <button className="h-[2.4rem] w-[2.4rem]">
-            <img className="h-[2.4rem] w-[2.4rem]" src={home} alt="" />
-          </button>
-          <div className="font-cookierun_m text-[1rem]">홈으로</div>
-        </div>
+        <Link to="/">
+          <div className="flex flex-col items-center space-y-[0.3rem]">
+            <button className="h-[3rem] w-[3rem] ml-[2rem]">
+              <img className="h-[3rem] w-[3rem]" src={home} alt="" />
+            </button>
+          </div>
+        </Link>
       </div>
     );
   }
   return (
     <div className="inline-flex w-[80%] items-center self-center place-content-center place-content-between">
       <div className="flex flex-col items-center  space-y-[0.3rem]">
-        <button className="h-[2.4rem] w-[2.4rem] ">
-          <img className="h-[2.4rem] w-[2.4rem]" src={share} alt="" />
+        <button onClick={onClick} className="h-[3rem] w-[3rem] ml-[17rem]">
+          <img className="h-[3rem] w-[3rem]" src={restart} alt="" /> {/* 다시하기 */}
         </button>
-
-        <div className="font-cookierun_m text-[1rem]">저장하기</div>
       </div>
-      <div className="flex flex-col items-center  space-y-[0.3rem]">
-        <button onClick={onClick} className="h-[2.4rem] w-[2.4rem]">
-          <img className="h-[2.4rem] w-[2.4rem]" src={changeImg} alt="" />
-        </button>
-        <div className="font-cookierun_m text-[1rem]">다시하기</div>
-      </div>
-      <div className="flex flex-col items-center space-y-[0.3rem]">
-        <button className="h-[2.4rem] w-[2.4rem]">
-          <img className="h-[2.4rem] w-[2.4rem]" src={home} alt="" />
-        </button>
-        <div className="font-cookierun_m text-[1rem]">홈으로</div>
-      </div>
+      <Link to="/">
+        <div className="flex flex-col items-center space-y-[0.3rem]">
+          <button className="h-[3rem] w-[3rem] ml-[2rem]">
+            <img className="h-[3rem] w-[3rem]" src={home} alt="" />
+          </button>
+        </div>
+      </Link>
     </div>
   );
 }
