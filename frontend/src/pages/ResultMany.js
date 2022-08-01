8f6e25fe52b405f92b0eef6be219064cf6aabab4
김하린 <halin130@naver.com>
2022-08-01 21:35:06 +0900

    feat: delete y-scroll

--- frontend/src/pages/ResultMany.js
+++ frontend/src/pages/ResultMany.js
@@ -80,9 +80,14 @@ function ResultMany() {
   }, [infoLoading, picLoading]);
 
   return (
+<<<<<<< develop
     <div id="resultmanypage" className={`relative flex w-screen h-screen ${isMobile ? 'bg-primary' : 'bg-primary-1'}`}>
       <GameBGImg pageName="ResultMany" />
       <div className="px-[3rem] deskTop:py-[2rem] mobile:pt-[8vh] w-full h-[100vh] absolute">
+=======
+    <div id="resultmanypage" className={`flex w-screen h-screen ${isMobile ? 'bg-primary' : 'bg-primary-1'}`}>
+      <div id={`${isMobile ? 'mobileBGImg' : 'BGyellowImg'}`} className="px-[3rem] py-[2rem] w-full h-full ">
+>>>>>>> feat: delete y-scroll
         <h1
           className="text-black font-cookierun text-left
           deskTop:text-5xl mobile:text-3xl my-[4rem] deskTop:ml-[4rem] mobile:text-center mobile:my-[2rem]"
@@ -109,7 +114,7 @@ function ResultMany() {
           </div>
         )}
         {isPc && infoLoading && picLoading && (
-          <div className="flex flex-wrap place-content-around w-[85%] h-[70%] justify-center m-auto">
+          <div className="flex flex-wrap place-content-around w-[85%] h-[60%] justify-center m-auto">
             {playersInfo.map((player, index) => (
               <ResultMulti
                 rank={index + 1}
