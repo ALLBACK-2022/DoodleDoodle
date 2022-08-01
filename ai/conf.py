79440d8e8174722953f9386a64693e225a4b3563
JeongHunHui <108508730+JeongHunHui@users.noreply.github.com>
2022-08-01 18:10:09 +0900

    Delete conf.py

--- ai/conf.py
+++
@@ -1,8 +0,0 @@
-# AWS_ACCESS_KEY = "AKIAYSQ5CFXD3U63YFPA"
-# AWS_SECRET_KEY = "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"
-# BUCKET_NAME = "doodle-bucket"
-# BUCKET_REGION = "ap-northeast-2"
-AWS_ACCESS_KEY = "AKIAYSQ5CFXDXXOH6B7X"
-AWS_SECRET_KEY = "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"
-BUCKET_NAME = "doodle-bucket"
-BUCKET_REGION = "ap-northeast-2"
