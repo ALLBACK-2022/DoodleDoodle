69116421239c407044ce7fb2b00adb8f3bcde62c
Hanjione <gkswldnj0803@gmail.com>
2022-08-02 12:02:19 +0900

    feat: api implementation
    /api/v1/task_status
    /api/v1/result_predict

--- ai/app.py
+++ ai/app.py
@@ -19,16 +19,16 @@ RABBITMQ_DEFAULT_PASS = os.environ.get("RABBITMQ_DEFAULT_PASS")
 os.chdir('/ai')
 
 
-def _is_complete(task_ids):
-    for task_id in task_ids:
-        status = celery_app.AsyncResult(task_id, app=celery_app)
-        if not (str(status.state) == "SUCCESS" or str(status.state) == "FAILURE"):
-            return str(status.state)
-        elif status == "FAILURE":
-            break
-    else:
-        return 'SUCCESS'
-    return 'FAILURE'
+# def _is_complete(task_ids):
+#     for task_id in task_ids:
+#         status = celery_app.AsyncResult(task_id, app=celery_app)
+#         if not (str(status.state) == "SUCCESS" or str(status.state) == "FAILURE"):
+#             return str(status.state)
+#         elif status == "FAILURE":
+#             break
+#     else:
+#         return 'SUCCESS'
+#     return 'FAILURE'
 
 
 def make_celery(app):
@@ -64,64 +64,95 @@ app.config.update(
 
 celery_app = make_celery(app)
 
-
+#AI 작업 요청
 @app.route('/api/v1/start_predict', methods=['POST'])
 def call_method():
     value = request.get_json()
-    draw_id = value['draw_id']
+    img = value['img']
     ranword = value['ranword']
 
+    #이미지에 따라서 수정 필요
     task = celery_app.send_task('ai_predict', kwargs={
-        'draw_id': draw_id, 'ranword': ranword})
+        'img': img, 'ranword': ranword})
 
     task_id = task.id
-    rettaskid = {"task_id": task_id}
+    isTaskid = {"task_id": task_id}
 
-    return rettaskid
+    return isTaskid
+# @app.route('/api/v1/start_predict', methods=['POST'])
+# def call_method():
+#     value = request.get_json()
+#     draw_id = value['draw_id']
+#     ranword = value['ranword']
 
+#     task = celery_app.send_task('ai_predict', kwargs={
+#         'draw_id': draw_id, 'ranword': ranword})
 
+#     task_id = task.id
+#     rettaskid = {"task_id": task_id}
+
+#     return rettaskid
+
+#상태 조회
 @app.route('/api/v1/task_status', methods=['POST'])
 def get_status():
-
-    # 상태 조회 제한 시간
-    start = time.time()
-
-    status = {"STARTED": 1, "PENDING": 1,
-              "FAILURE": 0, "SUCCESS": 0, "RETRY": 1}
     response_data = request.get_json()
-    task_ids = response_data["task-id"]
-    res, temp_str = 1, ""
-    while (res):
-        temp_str = _is_complete(task_ids)
-        res = status[str(temp_str)]
-        time.sleep(1.0)
-        # time.sleep(35.0)
-        end = time.time()
-        if((end-start) > 30):
-            print('시간이 초과되었습니다!!')
-            return {"status": "FAILURE"}
+    task_id = response_data["task-id"]
+    status = celery_app.AsyncResult(task_id, app=celery_app)
+    
+    isStatus ={"status" : str(status.state)}
+    return isStatus
 
-    return {"status": temp_str}
+# @app.route('/api/v1/task_status', methods=['POST'])
+# def get_status():
+
+#     # 상태 조회 제한 시간
+#     start = time.time()
+
+#     status = {"STARTED": 1, "PENDING": 1,
+#               "FAILURE": 0, "SUCCESS": 0, "RETRY": 1}
+#     response_data = request.get_json()
+#     task_ids = response_data["task-id"]
+#     res, temp_str = 1, ""
+#     while (res):
+#         temp_str = _is_complete(task_ids)
+#         res = status[str(temp_str)]
+#         time.sleep(1.0)
+#         # time.sleep(35.0)
+#         end = time.time()
+#         if((end-start) > 30):
+#             print('시간이 초과되었습니다!!')
+#             return {"status": "FAILURE"}
+
+#     return {"status": temp_str}
 
 
-def _is_complete(task_ids):
-    for task_id in task_ids:
-        status = celery_app.AsyncResult(task_id, app=celery_app)
-        print('status', status.state)
-        print('task-id', status)
-        if not (str(status.state) == "SUCCESS" or str(status.state) == "FAILURE"):
-            return str(status.state)
-        elif status.state == "FAILURE":
-            break
-    else:
-        return 'SUCCESS'
-    return 'FAILURE'
+# def _is_complete(task_ids):
+#     for task_id in task_ids:
+#         status = celery_app.AsyncResult(task_id, app=celery_app)
+#         print('status', status.state)
+#         print('task-id', status)
+#         if not (str(status.state) == "SUCCESS" or str(status.state) == "FAILURE"):
+#             return str(status.state)
+#         elif status.state == "FAILURE":
+#             break
+#     else:
+#         return 'SUCCESS'
+#     return 'FAILURE'
 
+#결과 반환
+@app.route('/api/v1/result_predict', methods=['POST'])
+def task_result():
+    response_data = request.get_json()
+    task_id = response_data["task-id"]
+    result = str(celery_app.AsyncResult(task_id).result)
+    isResult ={"result": result}
+    return isResult
 
-@app.route('/simple_task_result/<task_id>', methods=['GET'])
-def task_result(task_id):
-    ret = celery_app.AsyncResult(task_id).result
-    return str(ret)
+# @app.route('/simple_task_result/<task_id>', methods=['GET'])
+# def task_result(task_id):
+#     ret = celery_app.AsyncResult(task_id).result
+#     return str(ret)
 
 
 if __name__ == '__main__':
