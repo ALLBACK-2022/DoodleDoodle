2ea9e964f1815e457c65bbbe86bc6c3392ada1db
YunHo Jung <a01049048063@gmail.com>
2022-08-02 20:56:02 +0900

    feat: fix results/game/<int:gameid> api

--- backend/app.py
+++ backend/app.py
@@ -16,7 +16,6 @@ from flask_migrate import Migrate
 from sqlalchemy_utils import database_exists, create_database
 from sqlalchemy import and_
 
-
 app = Flask(__name__)
 load_dotenv()
 CORS(app, resources={r"/api/*": {"origins": "*"}}, supports_credentials=True)
