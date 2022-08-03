21d9b4b83df68195859cefcb96f5e28b42874b4f
Dayeon-Hong <dayon0325@gmail.com>
2022-08-03 15:16:49 +0900

    feat: loading page

--- frontend/src/components/Loading.js
+++ frontend/src/components/Loading.js
@@ -18,11 +18,11 @@ function Loading() {
   //   });
 
   return (
-    <div className="absolute deskTop:pl-[40rem] deskTop:pt-[20rem] mobile:pl-[10rem] mobile:pt-[25rem]">
-      <div className="flex-row">
+    <div className="absolute left-[50vw] top-[50vh] translate-x-[-50%] translate-y-[-50%]">
+      <div className="inline-flex text-center flex-col justify-center items-center">
         {/* <div id="container" style={{ width: '200px', height: '200px' }} /> */}
         <TailSpin color="#ea580c" height={80} width={80} />
-        <h2 className="item-center mt-[2rem] font-cookierun_m text-orange-600 deskTop:text-3xl mobile:text-xl">
+        <h2 className="item-center mt-[1rem] font-cookierun_m text-orange-600 deskTop:text-3xl mobile:text-xl">
           사진 분석 중
         </h2>
       </div>
